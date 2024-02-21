54aee5b0e3e12e3a5ea00dee7983ee53c35070c2
QAEZZ <russian.devv@gmail.com>
2024-02-21 12:48:47 -0600

    oh hell naw

---
+++ 1. Runde_files/cb=gapi_002.js
@@ -0,0 +1,4 @@
+gapi.loaded_1(function(_){var window=this;
+_.C("gapi.rpc.setup",_.om);_.C("gapi.rpc.register",_.dm);_.C("gapi.rpc.call",_.hm);_.C("gapi.rpc.unregister",_.em);_.C("gapi.rpc.sendHandshake",function(a,b){_.om(a)();_.Zl.send(a,_.Zl.MI(b),"*")});
+});
+// Google Inc.
